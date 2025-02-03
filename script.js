16f8459d49c7c90c6c43c59ed88711c3aa533ef9
Precious <precious.tochi.ikechukwu@gmail.com>
2025-02-03 15:20:34 +0100

    contents

--- script.js
+++ script.js
@@ -20,18 +20,5 @@ function updateTime() {
     timeElement.textContent = `Current UTC Time: ${currentTime}`;
 }
 
-function updateDate() {
-        const dateElement = document.getElementById('date');
-        const currentDate = new Date().toLocaleDateString();
-        dateElement.textContent = currentDate;
-    }
-    
     updateTime();
-    // function updateTime() {
-    //     const timeElement = document.getElementById('currentTime');
-    //     const currentTime = new Date().toUTCString();
-    //     timeElement.textContent = `Current UTC Time: ${currentTime}`;
-    // }
-    // updateDate(); // Initialize with current date
-    
-    setInterval(updateTime, 1000); // Update time every second
+    setInterval(updateTime, 1000);
