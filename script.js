1ddb41b7e2899599d4f09d997c2c4645252aabd9
Precious <precious.tochi.ikechukwu@gmail.com>
2025-02-03 14:58:53 +0100

    image update

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const tiltBox = document.querySelector(".wrapper");
+
+//     tiltBox.addEventListener("mousemove", (e) => {
+//         const { width, height, left, top } = tiltBox.getBoundingClientRect();
+//         const x = (e.clientX - left) / width - 0.5;
+//         const y = (e.clientY - top) / height - 0.5;
+
+//         tiltBox.style.transform = `rotateY(${x * 30}deg) rotateX(${-y * 30}deg)`;
+//     });
+
+//     tiltBox.addEventListener("mouseleave", () => {
+//         tiltBox.style.transform = "rotateY(0deg) rotateX(0deg)";
+//     });
+// });
+
 function updateTime() {
     const timeElement = document.getElementById('currentTime');
     const currentTime = new Date().toUTCString();
